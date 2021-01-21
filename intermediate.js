be70c3f76bfd08150a03a9a7951c8b8b5ef0bc0d
Bruno Seidi Koura <65494771+BrunoSeidi@users.noreply.github.com>
2021-01-21 15:29:17 -0300

    Add files via upload

---
+++ intermediate.js
@@ -0,0 +1,28 @@
+//objects
+//store data into an object instead of an array, objects can be more organized when have more info to give
+
+const fitBitData = {
+    steps : 308727,
+    miles : 211.7,
+    calories: 5755,
+    workout: '5 of 7',
+    avgSleep: '2:13'
+};
+
+//if i wanna access some data inside the const value
+//ex:
+fitBitData.miles //just access
+//keys are stored as strings
+//but i can use []
+//ex:
+const number = {
+    100: "one hundred",
+    16: "sixteen"
+};
+
+//console: number[100] = "one hundred"
+
+
+
+
+   
