19c6dfd72a886b2df5b79f20d1c676eb17bf0e23
Bruno Seidi Koura <65494771+BrunoSeidi@users.noreply.github.com>
2021-02-20 14:08:22 -0300

    Update jsnewbie.js

--- jsnewbie.js
+++ jsnewbie.js
@@ -51,7 +51,11 @@ room.reverse() // will reverse all the list
 room.join('.')// will put every thing into a same string separated by "."
 
 room.splice(start(posição do elemento), delete, add);//add a value or delete in the middle of the array
+//use
 
 
 //sort
 room.sort(); //turns all the elements into strings, so it gets worng when using number
+
+
+
